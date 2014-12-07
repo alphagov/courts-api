e7a09c1c53dc31b4f496b74cc8273bda4810f0e9
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-12-07 22:44:24 +0000

    Validate against a simple JSON schema for PUT request
    - return 422 if validation against the schema fails
    - return 500 if the schema is invalid
    
    This also defines a custom exception for the 422 status code; this
    status doesn't have an error class already in Falcon (perhaps because
    it's defined in a different RFC?).

--- courts.py
+++ courts.py
@@ -1,10 +1,40 @@
 import falcon
 import json
 
+from falcon.http_error import HTTPError
+from jsonschema import validate, ValidationError, SchemaError
+
+
+class HTTPUnprocessableEntity(HTTPError):
+    """422 Unprocessable Entity.
+
+    The server understands the content type of the request entity, and the
+    syntax of the request entity is correct, but was unable to process the
+    contained instructions. (RFC 4918)
+    Args:
+        title (str): Error title (e.g., 'Required fields are missing').
+        description (str): Human-friendly description of the error, along with
+            a helpful suggestion or two.
+        kwargs (optional): Same as for ``HTTPError``.
+    """
+
+    def __init__(self, title, description, **kwargs):
+        HTTPError.__init__(self, '422 Unprocessable Entity', title, description, **kwargs)
+
 
 def check_client_accepts_json(req, resp, params):
     if not req.client_accepts_json:
         raise falcon.HTTPNotAcceptable('You must accept JSON')
+
+
+def validate_court(data):
+    with open('court_schema.json') as schema:
+        try:
+            validate(data, json.load(schema))
+        except SchemaError:
+            raise falcon.HTTPInternalServerError
+        except ValidationError as e:
+            raise HTTPUnprocessableEntity('Validation failed', e.message)
 
 
 @falcon.before(check_client_accepts_json)
@@ -21,6 +51,8 @@ class CourtResource(object):
 
     def on_put(self, req, resp, uuid):
         data = json.load(req.stream)
+        validate_court(data)
+
         resp.body = json.dumps({
             'status': 'created',
             'uuid': uuid,
