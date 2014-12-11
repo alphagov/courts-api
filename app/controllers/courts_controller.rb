1b1116f869c65530847522780c4dbac6a0f192cd
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2014-12-11 14:37:16 +0000

    Send a court to the publishing API
    This is as basic as humanly possible while still sending something valid
    to the publishing API (lumping everything into the controller rather
    than adding a separate model layer, for example), but it works.

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,6 +1,34 @@
 class CourtsController < ApplicationController
   def update
     court_body = JSON.parse(request.body.read)
+    publishing_api_body = publishing_api_format(params[:id], court_body)
+
+    CourtsAPI.publishing_api.put_content_item(
+      base_path(court_body),
+      publishing_api_body,
+    )
     render json: {name: court_body["name"]}
+  end
+
+private
+  def base_path(court_body)
+    "/courts/#{court_body["slug"]}"
+  end
+
+  def publishing_api_format(court_id, court_body)
+    name = court_body["name"]
+
+    {
+      "base_path" => base_path(court_body),
+      "content_id" => court_id,
+      "title" => name,
+      "format" => "court",
+      "update_type" => "major",
+      "publishing_app" => "courts-api",
+      "rendering_app" => "courts-frontend",
+      "routes" => [
+        {"path" => base_path(court_body), "type" => "exact"}
+      ]
+    }
   end
 end
--- spec/requests/courts_spec.rb
+++ spec/requests/courts_spec.rb
@@ -1,17 +1,47 @@
 require 'rails_helper'
 
 describe 'publishing a court' do
-  let(:court_json) {
-    {"name" => "Barnsley Squash Court"}.to_json
-  }
+
+  let(:court_json) do
+    {"name" => "Barnsley Squash Court", "slug" => "barnsley-squash-court"}.to_json
+  end
+
+  let(:publishing_api_hash) do
+    {
+      "base_path" => "/courts/barnsley-squash-court",
+      "content_id" => court_id,
+      "title" => "Barnsley Squash Court",
+      "format" => "court",
+      "update_type" => "major",
+      "publishing_app" => "courts-api",
+      "rendering_app" => "courts-frontend",
+      "routes" => [
+        {
+          "path" => "/courts/barnsley-squash-court",
+          "type" => "exact",
+        }
+      ]
+    }
+  end
+
+  let(:court_id) { '0c833dee-01de-4f4d-9b2e-e7d1b8d76418' }
 
   it 'responds with a success code' do
-    put '/courts/0c833dee-01de-4f4d-9b2e-e7d1b8d76418', court_json
+    put "/courts/#{court_id}", court_json
     expect(response).to be_success
   end
 
+  it 'sends the court to the publishing API' do
+    expect(CourtsAPI.publishing_api).to receive(:put_content_item).with(
+      "/courts/barnsley-squash-court",
+      publishing_api_hash
+    )
+
+    put "/courts/#{court_id}", court_json
+  end
+
   it 'includes the court name in the response' do
-    put '/courts/0c833dee-01de-4f4d-9b2e-e7d1b8d76418', court_json
+    put "/courts/#{court_id}", court_json
     response_json = JSON.parse(response.body)
     expect(response_json).to include("name")
     expect(response_json["name"]).to eq("Barnsley Squash Court")
