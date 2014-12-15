200c97640b33d1a5c9291b31db01cf2a4084c470
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-12-15 17:25:44 +0000

    RSpec-ify courts_spec
    * Use contexts
    * Use have_received matcher to avoid `put` duplication
    * Remove second assertions on response field specs

--- spec/requests/courts_spec.rb
+++ spec/requests/courts_spec.rb
@@ -1,75 +1,72 @@
 require 'rails_helper'
 
 describe 'publishing a court' do
-
-  let(:court_json) do
-    { name: 'Barnsley Squash Court', slug: 'barnsley-squash-court' }.to_json
-  end
-
-  let(:publishing_api_hash) do
-    {
-      base_path: '/courts/barnsley-squash-court',
-      content_id: court_id,
-      title: 'Barnsley Squash Court',
-      format: 'court',
-      update_type: 'major',
-      publishing_app: 'courts-api',
-      rendering_app: 'courts-frontend',
-      routes: [
-        {
-          path: '/courts/barnsley-squash-court',
-          type: 'exact',
-        }
-      ]
-    }
-  end
-
   let(:court_id) { '0c833dee-01de-4f4d-9b2e-e7d1b8d76418' }
 
-  before :each do
+  before do
     allow(CourtsAPI.publishing_api).to receive(:put_content_item)
-  end
-
-  it 'responds with a success code' do
-    put "/courts/#{court_id}", court_json
-    expect(response).to have_http_status(200)
-  end
-
-  it 'sends the court to the publishing API' do
-    expect(CourtsAPI.publishing_api).to receive(:put_content_item).with(
-      '/courts/barnsley-squash-court',
-      publishing_api_hash
-    )
-
     put "/courts/#{court_id}", court_json
   end
 
-  it 'includes the court name in the response' do
-    put "/courts/#{court_id}", court_json
-    response_json = JSON.parse(response.body)
-    expect(response_json).to include('name')
-    expect(response_json['name']).to eq('Barnsley Squash Court')
+  context 'the happy path' do
+    let(:court_json) do
+      { name: 'Barnsley Squash Court', slug: 'barnsley-squash-court' }.to_json
+    end
+
+    let(:publishing_api_hash) do
+      {
+        base_path: '/courts/barnsley-squash-court',
+        content_id: court_id,
+        title: 'Barnsley Squash Court',
+        format: 'court',
+        update_type: 'major',
+        publishing_app: 'courts-api',
+        rendering_app: 'courts-frontend',
+        routes: [
+                     {
+                       path: '/courts/barnsley-squash-court',
+                       type: 'exact',
+                     }
+                   ]
+      }
+    end
+
+    let(:response_json) { JSON.parse(response.body) }
+
+    it 'responds with a success code' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'sends the court to the publishing API' do
+      expect(CourtsAPI.publishing_api).to have_received(:put_content_item).with(
+                                            '/courts/barnsley-squash-court',
+                                            publishing_api_hash
+                                          )
+    end
+
+    it 'includes the court name in the response' do
+      expect(response_json['name']).to eq('Barnsley Squash Court')
+    end
+
+    it 'includes the published URL in the response' do
+      expect(response_json['public_url']).to eq('https://www.gov.uk/courts/barnsley-squash-court')
+    end
   end
 
-  it 'includes the published URL in the response' do
-    put "/courts/#{court_id}", court_json
-    response_json = JSON.parse(response.body)
-    expect(response_json).to include('public_url')
-    expect(response_json['public_url']).to eq('https://www.gov.uk/courts/barnsley-squash-court')
-  end
+  context 'the unhappy endings' do
+    context 'no name is given' do
+      let(:court_json) { { slug: 'barnsley-squash-court' }.to_json }
+      it('is unprocessable') { expect(response).to have_http_status(422) }
+    end
 
-  it 'requires a name' do
-    put "/courts/#{court_id}", { slug: 'barnsley-squash-court' }.to_json
-    expect(response).to have_http_status(422)
-  end
+    context 'no slug is given' do
+      let(:court_json) { { name: 'Barnsley Squash Court' }.to_json }
+      it('is unprocessable') { expect(response).to have_http_status(422) }
+    end
 
-  it 'requires a slug' do
-    put "/courts/#{court_id}", { name: 'Barnsley Squash Court' }.to_json
-    expect(response).to have_http_status(422)
-  end
-
-  it 'returns 400 for invalid JSON' do
-    put "/courts/#{court_id}", '{"trailing": "comma",}'
-    expect(response).to have_http_status(400)
+    context 'the JSON is invalid' do
+      let(:court_json) { '{"trailing": "comma",}' }
+      it('is a bad request') { expect(response).to have_http_status(400) }
+    end
   end
 end
