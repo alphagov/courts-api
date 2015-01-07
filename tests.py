a3ad793423b1f867986dfff49d9c558ad11a624a
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-07 14:40:12 +0000

    Factor out request helper methods and constants

--- tests.py
+++ tests.py
@@ -1,9 +1,23 @@
 import falcon
-import uuid
+from uuid import uuid4 as random_uuid
 from falcon.testing import TestBase
 from json import dumps
 
 from app.app import courts_api
+
+
+def court_path(court_uuid):
+    return '/courts/{}'.format(court_uuid)
+
+VALID_REQUEST_HEADERS = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+VALID_REQUEST_BODY = {
+    'name': 'Barnsley Court',
+    'slug': 'barnsley-court'
+}
 
 
 class CourtsAPITestBase(TestBase):
@@ -11,23 +25,19 @@ class CourtsAPITestBase(TestBase):
         super(CourtsAPITestBase, self).setUp()
         self.api = courts_api
 
+    def put(self, body, headers={}, court_uuid=None):
+        merged_headers = dict(VALID_REQUEST_HEADERS.items() + headers.items())
+        if not court_uuid:
+            court_uuid = random_uuid()
+        return self.simulate_request(
+            court_path(court_uuid),
+            method='PUT',
+            headers=merged_headers,
+            body=dumps(body)
+        )
+
 
 class CourtRequestTests(CourtsAPITestBase):
     def test_putting_a_valid_court(self):
-        court_uuid = uuid.uuid4()
-        request_headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-        request_body = {
-            'name': 'Barnsley Court',
-            'slug': 'barnsley-court'
-        }
-        self.simulate_request(
-            '/courts/{}'.format(court_uuid),
-            method='PUT',
-            headers=request_headers,
-            body=dumps(request_body)
-        )
-
+        self.put(VALID_REQUEST_BODY)
         self.assertEqual(self.srmock.status, falcon.HTTP_201)
