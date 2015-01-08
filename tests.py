8a273aa060748f44d96ac9a7b22691974b1d41a7
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-08 17:44:50 +0000

    Add assertStatus helper to CourtsAPITestBase
    This makes the tests a bit easier to read.

--- tests.py
+++ tests.py
@@ -37,30 +37,34 @@ class CourtsAPITestBase(TestBase):
             body=dumps(body)
         )
 
+    def assertStatus(self, status):
+        "Compare the mock response status to a Falcon-style status code string"
+        return self.assertEqual(self.srmock.status, status)
+
 
 class HealthcheckTests(CourtsAPITestBase):
     def test_healthcheck(self):
         resp = self.simulate_request('/healthcheck', decode='utf-8')
-        self.assertEqual(self.srmock.status, falcon.HTTP_200)
+        self.assertStatus(falcon.HTTP_200)
         self.assertEqual(resp, 'OK')
 
 
 class CourtRequestTests(CourtsAPITestBase):
     def test_putting_a_valid_court(self):
         self.put(VALID_REQUEST_BODY)
-        self.assertEqual(self.srmock.status, falcon.HTTP_201)
+        self.assertStatus(falcon.HTTP_201)
 
     def test_putting_an_invalid_court(self):
         self.put({'foo': 'bar'})
-        self.assertEqual(self.srmock.status, HTTP_422)
+        self.assertStatus(HTTP_422)
 
     def test_putting_with_unsupported_media_type(self):
         self.put(VALID_REQUEST_BODY, headers={'Content-Type': 'text/plain'})
-        self.assertEqual(self.srmock.status, falcon.HTTP_415)
+        self.assertStatus(falcon.HTTP_415)
 
     def test_putting_without_accepting_json(self):
         self.put(VALID_REQUEST_BODY, headers={'Accept': 'application/xml'})
-        self.assertEqual(self.srmock.status, falcon.HTTP_406)
+        self.assertStatus(falcon.HTTP_406)
 
     def test_get_request_for_court_not_allowed(self):
         self.simulate_request(
@@ -69,7 +73,7 @@ class CourtRequestTests(CourtsAPITestBase):
             headers=VALID_REQUEST_HEADERS,
             body=dumps(VALID_REQUEST_BODY),
         )
-        self.assertEqual(self.srmock.status, falcon.HTTP_405)
+        self.assertStatus(falcon.HTTP_405)
 
     def test_get_all_courts(self):
         resp = self.simulate_request(
@@ -77,5 +81,5 @@ class CourtRequestTests(CourtsAPITestBase):
             method='GET',
             headers=VALID_REQUEST_HEADERS
         )
-        self.assertEqual(self.srmock.status, falcon.HTTP_200)
+        self.assertStatus(falcon.HTTP_200)
         self.assertIn('courts', loads(resp[0]))
