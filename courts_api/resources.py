e616e0607b8d0675558a3b9c94f2e12c35b34435
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-25 15:46:58 +0000

    Log a successful PUT request with extra fields
    We want to log all requests, regardless of whether they succeed or
    not, but for now this only handles the happy path. It includes some
    standard extra fields which are similar to those that other apps have
    in Kibana.

--- courts_api/resources.py
+++ courts_api/resources.py
@@ -1,9 +1,36 @@
 import falcon
 import json
+import logging
 
 from courts_api.plek import url_for_application
 from validators import (authenticate, check_client_is_sending_json,
     check_client_accepts_json, validate_court)
+
+
+logger = logging.getLogger(__name__)
+
+
+def request_log_field(req):
+    """Return a string representing the request for logging.
+
+    Eg 'PUT /courts/12345?foo=bar HTTP/1.1'
+    """
+    return '{0} {1} {2}'.format(
+        req.method,
+        req.relative_uri,
+        req.env['SERVER_PROTOCOL']
+    )
+
+def extra_log_data(req, resp, req_body):
+    """Return a dict of extra fields to log.
+    """
+    return {
+        'request': request_log_field(req),
+        'method': req.method,
+        'govuk_request_id': req.get_header('GOVUK-Request-Id'),
+        'request_body': req_body,
+        'status': resp.status, # FIXME make this just 201, not '201 Created'
+    }
 
 
 class HealthcheckResource(object):
@@ -28,7 +55,8 @@ class CourtsResource(object):
 class CourtResource(object):
 
     def on_put(self, req, resp, uuid):
-        data = json.load(req.stream)
+        req_body = req.stream.read()
+        data = json.loads(req_body)
         validate_court(data)
 
         print 'Sending to {}'.format(url_for_application('publishing-api'))
@@ -41,3 +69,5 @@ class CourtResource(object):
         })
         resp.location = 'https://www.preview.alphagov.co.uk/courts/{}'.format(data['slug'])
         resp.status = falcon.HTTP_201
+
+        logger.info('PUT a court', extra=extra_log_data(req, resp, req_body))
--- test/court_request_tests.py
+++ test/court_request_tests.py
@@ -8,24 +8,26 @@ from courts_api.errors import HTTP_422
 
 
 @patch('courts_api.signon.authenticate_api_user', new=Mock(return_value=True))
+@patch('courts_api.resources.logger')
 class CourtRequestTests(CourtsAPITestBase):
-    def test_putting_a_valid_court(self):
+    def test_putting_a_valid_court(self, logging_mock):
         self.put(VALID_REQUEST_BODY)
         self.assertStatus(falcon.HTTP_201)
+        self.assertTrue(logging_mock.info.called)
 
-    def test_putting_an_invalid_court(self):
+    def test_putting_an_invalid_court(self, logging_mock):
         self.put({'foo': 'bar'})
         self.assertStatus(HTTP_422)
 
-    def test_putting_with_unsupported_media_type(self):
+    def test_putting_with_unsupported_media_type(self, logging_mock):
         self.put(VALID_REQUEST_BODY, headers={'Content-Type': 'text/plain'})
         self.assertStatus(falcon.HTTP_415)
 
-    def test_putting_without_accepting_json(self):
+    def test_putting_without_accepting_json(self, logging_mock):
         self.put(VALID_REQUEST_BODY, headers={'Accept': 'application/xml'})
         self.assertStatus(falcon.HTTP_406)
 
-    def test_get_request_for_court_not_allowed(self):
+    def test_get_request_for_court_not_allowed(self, logging_mock):
         self.simulate_request(
             court_path(random_uuid()),
             method='GET',
@@ -33,7 +35,7 @@ class CourtRequestTests(CourtsAPITestBase):
         )
         self.assertStatus(falcon.HTTP_405)
 
-    def test_get_all_courts(self):
+    def test_get_all_courts(self, logging_mock):
         resp = self.simulate_request(
             '/courts',
             method='GET',
