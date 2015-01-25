9ff80c73e2609d6da14c12235c2608847cc64636
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-25 15:48:42 +0000

    Move logging setup into settings.py
    This needs to be executed early on so that the handler is added before
    the resource which makes calls to the logger is instantiated, so this
    commit imports it in courts_api/app.py which is the entry point to the
    application.

--- courts_api/app.py
+++ courts_api/app.py
@@ -1,20 +1,8 @@
 import falcon
-import logging
-from logstash_formatter import LogstashFormatter
-
 from resources import CourtsResource, CourtResource, HealthcheckResource
-from settings import govuk_env
+# Make sure that the logging config is executed early on:
+from settings import logger
 
-
-handler = logging.FileHandler("log/%s.json.log" % govuk_env())
-
-formatter = LogstashFormatter()
-handler.setFormatter(formatter)
-handler.setLevel(logging.INFO)
-
-logger = logging.getLogger('courts_api')
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
 
 application = falcon.API()
 
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import logging
+from logstash_formatter import LogstashFormatter
 import os
 
 from production_settings import *
@@ -8,3 +10,15 @@ def govuk_env():
 
 def app_domain():
     return os.getenv('GOVUK_APP_DOMAIN', 'dev.gov.uk')
+
+
+# Logging setup:
+handler = logging.FileHandler("log/%s.json.log" % govuk_env())
+
+formatter = LogstashFormatter()
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+
+logger = logging.getLogger('courts_api')
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
