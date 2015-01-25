f5b02a94be64a77fb9889e1b5fe46cd3a5a1d8b6
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-25 15:27:10 +0000

    Add basic logging config for logstash
    This sets up a file handler with the LogstashFormatter. Nothing is
    yet being logged.
    
    We need to ensure that the log/ directory exists; otherwise the app
    fails to boot because the handler is unable to write into it.

--- courts_api/app.py
+++ courts_api/app.py
@@ -1,7 +1,20 @@
 import falcon
+import logging
+from logstash_formatter import LogstashFormatter
 
 from resources import CourtsResource, CourtResource, HealthcheckResource
+from settings import govuk_env
 
+
+handler = logging.FileHandler("log/%s.json.log" % govuk_env())
+
+formatter = LogstashFormatter()
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+
+logger = logging.getLogger('courts_api')
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 application = falcon.API()
 
