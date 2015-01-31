a5c230f2e3a0c280a2b870d8d93330e7da92293b
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-31 20:01:44 +0000

    Add a Court class
    This factors out court-related methods from the CourtResource.

---
+++ test/court_tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+from courts_api.court import Court
+from helpers import random_uuid
+
+
+class CourtTestCase(unittest.TestCase):
+    def test_instantiation_sets_attributes(self):
+        uuid = random_uuid()
+        court = Court(uuid, {'name': 'Barnsley Court'})
+        self.assertEqual(uuid, court.uuid)
+        self.assertEqual('Barnsley Court', court.name)
+
+    def test_base_path(self):
+        court = Court(random_uuid(), {'slug': 'barnsley-court'})
+        self.assertEqual('/courts/barnsley-court', court.base_path)
+
+    def test_public_url(self):
+        court = Court(random_uuid(), {'slug': 'barnsley-court'})
+        self.assertEqual(
+            'http://www.dev.gov.uk/courts/barnsley-court',
+            court.public_url
+        )
+
+    def test_publishing_api_format(self):
+        uuid = random_uuid()
+        name = 'Barnsley Court'
+        slug = 'barnsley-court'
+        expected = {
+            'base_path': '/courts/barnsley-court',
+            'content_id': uuid,
+            'title': name,
+            'format': 'court',
+            'update_type': 'major',
+            'publishing_app': 'courts-api',
+            'rendering_app': 'courts-frontend',
+            'routes': [
+                {'path': '/courts/barnsley-court', 'type': 'exact'}
+            ]
+        }
+        self.assertEqual(
+            expected,
+            Court(uuid, {'name': name, 'slug': slug}).publishing_api_format
+        )
