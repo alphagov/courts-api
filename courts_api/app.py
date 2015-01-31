58f2c21080a52984db19149f46b701f02fa839d8
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-31 19:29:39 +0000

    Import from courts_api package explicitly in app.py

--- courts_api/app.py
+++ courts_api/app.py
@@ -1,6 +1,7 @@
 import falcon
-from middleware import ResponseLoggerMiddleware
-from resources import CourtsResource, CourtResource, HealthcheckResource
+
+from courts_api.middleware import ResponseLoggerMiddleware
+from courts_api.resources import CourtsResource, CourtResource, HealthcheckResource
 # Make sure that the logging config is executed early on:
 from settings import logger
 
