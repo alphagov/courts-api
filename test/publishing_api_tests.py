f1d0ec18c19721af5438211b715b55e158a37e06
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-31 17:11:34 +0000

    Add tests for PublishingAPI.put

--- test/publishing_api_tests.py
+++ test/publishing_api_tests.py
@@ -1,3 +1,6 @@
+import falcon
+from mock import patch, Mock
+from requests import ConnectionError
 import unittest
 
 from courts_api.publishing_api import PublishingAPI
@@ -11,3 +14,18 @@ class PublishingAPITestCase(unittest.TestCase):
             expected,
             PublishingAPI._url_for_data(data)
         )
+
+    @patch('requests.put')
+    def test_successful_put(self, put_mock):
+        put_mock.return_value = Mock(status_code=201)
+        data = {'base_path': '/courts/barnsley-court'}
+        resp = PublishingAPI.put(data)
+        self.assertEqual(201, resp.status_code)
+
+    @patch('requests.put')
+    def test_put_with_connection_error(self, put_mock):
+        put_mock.side_effect = ConnectionError('Connection aborted.')
+        data = {'base_path': '/courts/barnsley-court'}
+        with self.assertRaises(falcon.HTTPServiceUnavailable):
+            PublishingAPI.put(data)
+        self.assertTrue(put_mock.called)
