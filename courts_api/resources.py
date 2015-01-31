93bd1d1a05d13c6af963f906fc4cbaa860ea7a7b
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-31 19:02:33 +0000

    Factor out methods to update the response
    This simplifies the on_put method and clarifies what is being set
    and how. It also revealed that some functionality wasn't tested, so
    this adds tests for those cases too.

--- courts_api/resources.py
+++ courts_api/resources.py
@@ -1,6 +1,7 @@
 import falcon
 import json
 
+from courts_api.errors import HTTP_422
 from courts_api.plek import url_for_application
 from courts_api.publishing_api import PublishingAPI
 from validators import (authenticate, check_client_is_sending_json,
@@ -36,25 +37,10 @@ class CourtResource(object):
             self._court_publishing_api_format(uuid, data)
         )
 
-        if publishing_api_response.status_code in [200, 201]:
-            resp.body = json.dumps({
-                'name': data['name'],
-                'public_url': self._public_url(data),
-            })
-            resp.status = getattr(falcon, 'HTTP_{}'.format(publishing_api_response.status_code))
-            if publishing_api_response.status_code == 201:
-                # This is the URL to use for future updates to this court (it's
-                # the same as the one used for the current request):
-                resp.location = '{0}/courts/{1}'.format(
-                    url_for_application('courts-api'),
-                    uuid,
-                )
-        else:
-            raise falcon.HTTPServiceUnavailable(
-                'Temporarily Unavailable',
-                'Please try again later',
-                30  # value for Retry-After header
-            )
+        self._set_response_status(resp, publishing_api_response.status_code)
+        self._set_response_body(resp, publishing_api_response, data)
+        self._set_response_location_header(resp, publishing_api_response, uuid)
+
 
     @staticmethod
     def _base_path(court_body):
@@ -76,3 +62,47 @@ class CourtResource(object):
                 {'path': self._base_path(court_body), 'type': 'exact'}
             ]
         }
+
+    @staticmethod
+    def _set_response_status(resp, status_code):
+        """Set resp.status based on the given status code."""
+        if status_code == 422:
+            resp.status = HTTP_422
+        else:
+            resp.status = getattr(falcon, 'HTTP_{}'.format(status_code))
+
+    def _set_response_body(self, resp, publishing_api_response, court_data):
+        """Set the response body.
+
+        If the response from the publishing API was 4xx, include any returned
+        errors in resp.body. If the court was successfully sent to the
+        publishing API, include basic details about it instead.
+        """
+        publishing_api_resp_body = publishing_api_response.json()
+        status_code = publishing_api_response.status_code
+
+        if status_code in [200, 201]:
+            resp.body = json.dumps({
+                'name': court_data['name'],
+                'public_url': self._public_url(court_data),
+            })
+        elif 400 <= status_code <= 499 and \
+                'errors' in publishing_api_resp_body:
+            resp.body = json.dumps({
+                'errors': publishing_api_resp_body['errors']}
+            )
+
+    @staticmethod
+    def _set_response_location_header(resp, publishing_api_response, uuid):
+        """Set the location header if the court has just been created.
+
+        This is the courts-api URL for the court which should be used for future
+        requests to update it, not the public URL at which it has been published.
+        """
+        if publishing_api_response.status_code == 201:
+            # This is the URL to use for future updates to this court (it's
+            # the same as the one used for the current request):
+            resp.location = '{0}/courts/{1}'.format(
+                url_for_application('courts-api'),
+                uuid,
+            )
--- test/court_request_tests.py
+++ test/court_request_tests.py
@@ -1,5 +1,5 @@
 import falcon
-from json import loads
+from json import loads, dumps
 from mock import patch, Mock
 
 from helpers import (CourtsAPITestBase, court_path, random_uuid,
@@ -13,7 +13,10 @@ from courts_api.errors import HTTP_422
 class CourtRequestTests(CourtsAPITestBase):
     def test_putting_a_valid_court(self, logger_mock, put_mock):
         put_mock.return_value = Mock(status_code=201)
-        resp = self.put(VALID_REQUEST_BODY)
+        # We need to make an assertion later involving the uuid, so set it
+        # explictly here:
+        uuid = random_uuid()
+        resp = self.put(VALID_REQUEST_BODY, court_uuid=uuid)
 
         self.assertStatus(falcon.HTTP_201)
         self.assertTrue(put_mock.called)
@@ -25,6 +28,10 @@ class CourtRequestTests(CourtsAPITestBase):
         self.assertEqual(
             'http://www.dev.gov.uk/courts/barnsley-court',
             response_body['public_url']
+        )
+        self.assertEqual(
+            'http://courts-api.dev.gov.uk/courts/{}'.format(uuid),
+            self.srmock.headers_dict['Location']
         )
 
     def test_putting_an_invalid_court(self, logger_mock, put_mock):
@@ -47,6 +54,21 @@ class CourtRequestTests(CourtsAPITestBase):
         self.assertStatus(falcon.HTTP_406)
         self.assertFalse(put_mock.called)
         self.assertTrue(logger_mock.info.called)
+
+    def test_putting_with_422_from_publishing_api(self, logger_mock, put_mock):
+        """We expected this to succeed, but content-store validation failed"""
+        put_mock.return_value = Mock(
+            status_code=422,
+            content=dumps({'errors': {'routes': ['are invalid']}})
+        )
+        resp = self.put(VALID_REQUEST_BODY)
+
+        self.assertStatus(HTTP_422)
+        self.assertTrue(put_mock.called)
+        self.assertTrue(logger_mock.info.called)
+
+        response_body = loads(resp[0])
+        self.assertEqual(['are invalid'], response_body['errors']['routes'])
 
     def test_get_request_for_court_not_allowed(self, logger_mock, put_mock):
         self.simulate_request(
