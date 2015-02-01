b747fc393199008b428d968da5de894c4bd67ea7
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-02-01 14:29:58 +0000

    Move ENVIRON constant into test helpers module
    We're going to need this in other test case classes too.

--- test/middleware_tests.py
+++ test/middleware_tests.py
@@ -2,22 +2,16 @@ from json import dumps
 import unittest
 
 from courts_api.middleware import request_log_field, extra_log_data
+from test.helpers import ENVIRON
 
 
 class LoggingMiddlewareHelperTests(unittest.TestCase):
-    ENVIRON = {
-        'REQUEST_METHOD': 'PUT',
-        'PATH_INFO': '/courts/12345',
-        'QUERY_STRING': '',
-        'SERVER_PROTOCOL': 'HTTP/1.1',
-    }
-
     def test_request_log_field_without_query_string(self):
         expected = 'PUT /courts/12345 HTTP/1.1'
-        self.assertEqual(expected, request_log_field(self.ENVIRON))
+        self.assertEqual(expected, request_log_field(ENVIRON))
 
     def test_request_log_field_with_query_string(self):
-        environ = self.ENVIRON.copy()
+        environ = ENVIRON.copy()
         environ['QUERY_STRING'] = 'foo=bar'
         expected = 'PUT /courts/12345?foo=bar HTTP/1.1'
         self.assertEqual(expected, request_log_field(environ))
@@ -31,10 +25,10 @@ class LoggingMiddlewareHelperTests(unittest.TestCase):
             'request_body': '{"name": "Barnsley Court"}',
             'status': 422
         }
-        self.assertEqual(expected, extra_log_data(self.ENVIRON, request_body, 422))
+        self.assertEqual(expected, extra_log_data(ENVIRON, request_body, 422))
 
     def test_extra_log_data_with_govuk_request_id(self):
-        environ = self.ENVIRON.copy()
+        environ = ENVIRON.copy()
         environ['HTTP_GOVUK_REQUEST_ID'] = 'abcdefg'
         request_body = dumps({'name': 'Barnsley Court'})
         self.assertEqual(
